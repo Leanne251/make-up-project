4f2d7d71f72716ce47254d834604b55fcc73e86c
Leanne251 <leannesmith251@gmail.com>
2022-08-10 18:04:17 +0100

    save

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit,  } from '@angular/core';
+
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
 
 @Component({
@@ -9,25 +10,78 @@ import { HttpService } from '../services/http.service';
 })
 export class ResultsComponent implements OnInit {
 
+  constructor(private httpService: HttpService,  private cd: ChangeDetectorRef) { }
+
+
+  get results(){
+    return this.httpService.immutableResults
+  }
+
+  get length(){
+    return this.httpService.immutableLength
+  }
 
 
 
-  @Input() item:any
-  results: any
+  pages = []
+  itemsPerPage= 25;
+  resultsInView = []
 
-
-  constructor() { }
 
   ngOnInit(): void {
 
+    this.httpService.results$.subscribe(
+      ()=>{
+        this.cd.markForCheck();
+        this.pages = []
+        this.getPages()
+        this.getPageResults(1)
+
+      }
+    )
+    console.log("items", this.itemsPerPage )
+  }
 
 
 
+  getPages(){
+    let number = Math.ceil(this.length / this.itemsPerPage)
+    for(let i=1; i < number+1; i++){
+      this.pages.push(i)
+    }
+  }
 
-  // this.httpService.resultData.subscribe(
-  //   (data:any) => console.log("results", data)
-  // )
+  getPageResults(page: number){
+    let higherNo = page * this.itemsPerPage
+    let lowerNo = higherNo - this.itemsPerPage
+    this.resultsInView =  this.results.slice(lowerNo, higherNo)
+    console.log("results",this.resultsInView)
+
 
   }
 
+onChangeItemsDisplayed(event){
+  this.itemsPerPage =  event.target.value
+  this.pages = []
+  this.getPages()
+  this.getPageResults(1)
+
 }
+
+}
+
+// select how many items you want on the page.
+// default is 25 as pre-set in comp.
+
+// choose a new value, this updates the variable.
+
+// now we need to work out how many pages there will be, if we select X number to display.
+// so create a new function?
+
+// so if we have 300 results (.length)
+// and we want to display X results.
+
+// we need to do length / results per page.
+// so thet getPages function.
+
+// then we need to split the data accordingly, so just call getPage Results 1
