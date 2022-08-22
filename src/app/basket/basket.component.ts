11f546a5212dcff0b165474a383b3bf7cea30be4
Leanne251 <leannesmith251@gmail.com>
2022-08-22 20:28:45 +0100

    totals work and buttons styled

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -3,81 +3,94 @@ import { map } from 'rxjs';
 import { HttpService } from '../services/http.service';
 
 @Component({
-  selector: 'app-basket',
-  templateUrl: './basket.component.html',
-  styleUrls: ['./basket.component.css'],
+	selector: 'app-basket',
+	templateUrl: './basket.component.html',
+	styleUrls: [ './basket.component.css' ]
 })
 export class BasketComponent implements OnInit {
-  // get basketData() {
-  //   return this.httpService.immutableBasket;
-  // }
+	// get basketData() {
+	//   return this.httpService.immutableBasket;
+	// }
 
-  basketData: any;
-  amount: number;
+	basketData: any;
+	amount: number;
+	totalItemAmount: number;
+	totalItemCost: number;
+	showTotals: boolean;
 
-  constructor(private httpService: HttpService) {}
+	constructor(private httpService: HttpService) {}
 
-  ngOnInit(): void {
-    this.getBasket();
+	ngOnInit(): void {
+		this.getBasket();
+	}
 
-    this.getTotalAmounts();
-  }
+	getBasket() {
+		this.httpService
+			.getBasket()
+			.pipe(
+				map((responseData) => {
+					const itemData = [];
+					for (const key in responseData) {
+						if (responseData.hasOwnProperty(key)) {
+							itemData.push({ ...responseData[key], firebase_id: key });
+						}
+					}
+					return itemData;
+				})
+			)
+			.subscribe((data) => {
+				this.basketData = data;
+				if (data.length > 0) {
+					this.showTotals = true;
+				} else {
+					this.showTotals = false;
+				}
+				this.getTotalItemAmount(data);
+			});
+	}
 
-  getBasket() {
-    this.httpService
-      .getBasket()
-      .pipe(
-        map((responseData) => {
-          const itemData = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              itemData.push({ ...responseData[key], firebase_id: key });
-            }
-          }
-          return itemData;
-        })
-      )
-      .subscribe((data) => {
-        this.basketData = data;
-        console.log('basket', this.basketData);
-      });
-  }
+	changeItemAmount(firebase_id: string, amount: number, changeType: string) {
+		if (changeType === 'add') {
+			amount++;
+		} else {
+			amount--;
+		}
 
-  changeItemAmount(firebase_id: string, amount: number, changeType: string) {
-    if (changeType === 'add') {
-      amount++;
-    } else {
-      amount--;
-    }
+		if (amount === 0) {
+			this.httpService.deleteItem(firebase_id).subscribe(() => {});
+		}
+		this.httpService.changeAmountViaFireBaseID(firebase_id, amount).subscribe(() => {
+			this.getBasket();
+		});
+	}
 
-    this.httpService
-      .changeAmountViaFireBaseID(firebase_id, amount)
-      .subscribe(() => {
-        this.getBasket();
-      });
-  }
+	deleteAll(firebase_id: string) {
+		this.httpService.deleteItem(firebase_id).subscribe(() => {
+			this.basketData = [];
+			this.getBasket();
+		});
+	}
 
-  deleteAll(firebase_id: string) {
-    this.httpService.deleteItem(firebase_id).subscribe(() => {
-      this.basketData = [];
-      this.getBasket();
-    });
-  }
+	getTotalItemAmount(data) {
+		let amountValues = [];
+		let costTotal = [];
+		data.forEach((el) => {
+			amountValues.push(el.amount);
+			let cost = el.amount * el.price;
+			costTotal.push(cost);
+		});
+		this.totalItemAmount = amountValues.reduce((p, c) => p + c, 0);
+		this.totalItemCost = costTotal.reduce((p, c) => p + c, 0);
 
-  getTotalAmounts() {
-    if (this.basketData) {
-      console.log(1);
-      let amountValues = [];
-      this.basketData.forEach((el) => {
-        console.log(el.amount);
-        amountValues.push(el.amount);
-      });
+		// let total = parseFloat(costTotal.reduce((p, c) => p + c, 0));
+		// this.totalItemCost = Math.round(total * 100) / 100;
+	}
 
-      console.log(amountValues, 'amount V');
-      amountValues.reduce((p, c) => p + c, 0);
-      console.log(amountValues, 'amount V');
-    }
-  }
+	emptyBasket() {
+		this.httpService.deleteAll().subscribe(() => {
+			this.getBasket();
+		});
+	}
 }
 
 // loop over the basket data and get all the values of the amount key
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,103 +3,103 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { map, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root'
 })
 export class HttpService {
-  get immutableResults() {
-    return this.results;
-  }
+	get immutableResults() {
+		return this.results;
+	}
 
-  get immutableLength() {
-    return this.length;
-  }
+	get immutableLength() {
+		return this.length;
+	}
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  private results: any;
-  private length: any;
+	private results: any;
+	private length: any;
 
-  params: any;
+	params: any;
 
-  results$ = new Subject();
+	results$ = new Subject();
 
-  getAllProducts() {
-    this.http
-      .get<any>('http://makeup-api.herokuapp.com/api/v1/products.json')
-      .subscribe((data: any) => {
-        this.results = data;
-        this.length = data.length;
-        this.results$.next(null);
-      });
-  }
+	getAllProducts() {
+		this.http.get<any>('http://makeup-api.herokuapp.com/api/v1/products.json').subscribe((data: any) => {
+			this.results = data;
+			this.length = data.length;
+			this.results$.next(null);
+		});
+	}
 
-  fetchDataWithParams(formData: any) {
-    let params = this.getParams(formData);
+	fetchDataWithParams(formData: any) {
+		let params = this.getParams(formData);
 
-    let url = 'http://makeup-api.herokuapp.com/api/v1/products.json';
+		let url = 'http://makeup-api.herokuapp.com/api/v1/products.json';
 
-    if (params) {
-      this.http.get(url, { params }).subscribe((data: any) => {
-        this.results = data;
-        this.length = data.length;
-        this.results$.next(null);
-      });
-    }
-  }
+		if (params) {
+			this.http.get(url, { params }).subscribe((data: any) => {
+				this.results = data;
+				this.length = data.length;
+				this.results$.next(null);
+			});
+		}
+	}
 
-  getParams(formData: any) {
-    this.params = new HttpParams();
+	getParams(formData: any) {
+		this.params = new HttpParams();
 
-    if (formData.brand !== null || '') {
-      this.params = this.params.set('brand', formData.brand);
-    }
+		if (formData.brand !== null || '') {
+			this.params = this.params.set('brand', formData.brand);
+		}
 
-    if (formData.product_type !== null || '') {
-      this.params = this.params.set('product_type', formData.product_type);
-    }
+		if (formData.product_type !== null || '') {
+			this.params = this.params.set('product_type', formData.product_type);
+		}
 
-    if (formData.product_tags.length > 0) {
-      let productTags = '';
-      for (let i = 0; i < formData.product_tags.length; i++) {
-        productTags += `${formData.product_tags[i].tag},`;
-      }
-      let productTagsParams = productTags.slice(0, -1);
-      this.params = this.params.set('product_tags', productTagsParams);
-    }
+		if (formData.product_tags.length > 0) {
+			let productTags = '';
+			for (let i = 0; i < formData.product_tags.length; i++) {
+				productTags += `${formData.product_tags[i].tag},`;
+			}
+			let productTagsParams = productTags.slice(0, -1);
+			this.params = this.params.set('product_tags', productTagsParams);
+		}
 
-    return this.params;
-  }
+		return this.params;
+	}
 
-  isLoading() {
-    this.results$.subscribe(() => {
-      return false;
-    });
-  }
+	isLoading() {
+		this.results$.subscribe(() => {
+			return false;
+		});
+	}
 
-  addNewItemToBasket(data) {
-    return this.http.post(
-      'https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up.json',
-      data
-    );
-  }
+	addNewItemToBasket(data) {
+		return this.http.post(
+			'https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up.json',
+			data
+		);
+	}
 
-  getBasket() {
-    return this.http.get(
-      'https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up.json'
-    );
-  }
+	getBasket() {
+		return this.http.get('https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up.json');
+	}
 
-  deleteItem(firebase_id: string) {
-    return this.http.delete(
-      `https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up/${firebase_id}.json`
-    );
-  }
+	deleteItem(firebase_id: string) {
+		return this.http.delete(
+			`https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up/${firebase_id}.json`
+		);
+	}
 
-  changeAmountViaFireBaseID(firebase_id: string, amount: number) {
-    let data = JSON.stringify({ amount: amount });
-    return this.http.patch(
-      `https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up/${firebase_id}.json`,
-      data
-    );
-  }
+	deleteAll() {
+		return this.http.delete(`https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up.json`);
+	}
+
+	changeAmountViaFireBaseID(firebase_id: string, amount: number) {
+		let data = JSON.stringify({ amount: amount });
+		return this.http.patch(
+			`https://make-up-f1a3e-default-rtdb.europe-west1.firebasedatabase.app/make-up/${firebase_id}.json`,
+			data
+		);
+	}
 }
